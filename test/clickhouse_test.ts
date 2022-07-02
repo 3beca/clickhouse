3c44e59b2a77a3e70d9043a35776c810d8c8cced
Francesco Rivola <francesco.rivola@gmail.com>
2022-07-02 17:27:47 +0200

    removed basic test created only during ttd

--- test/clickhouse_test.ts
+++ test/clickhouse_test.ts
@@ -1,26 +1,13 @@
-import {
-  assertEquals,
-  assertNotEquals,
-} from "https://deno.land/std@0.146.0/testing/asserts.ts";
+import { assertEquals } from "https://deno.land/std@0.146.0/testing/asserts.ts";
 import { ClickHouse } from "../mod.ts";
 
-Deno.test("ClickHouse is not undefined", () => {
-  assertNotEquals(ClickHouse, undefined);
-});
-
-Deno.test("ClickHouse should be a class with new", () => {
-  const clickHouse = new ClickHouse();
-  assertNotEquals(clickHouse, undefined);
-});
-
-Deno.test("ClickHouse should allow ping db", async () => {
+Deno.test("ClickHouse client should allow ping db", async () => {
   const clickHouse = new ClickHouse({
     protocol: "http",
     host: "localhost",
     port: 8123,
     path: "/",
   });
-
   const pingResult = await clickHouse.ping();
   assertEquals(pingResult, "Ok.\n");
 });
